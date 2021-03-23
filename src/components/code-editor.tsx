8139f2969f6bb5a4368dc441c155c70bcbcb255e
Ian Rones <ronesp.85@gmail.com>
2021-03-23 13:32:02 -0700

    [edit] refactored components, created a preview file that holds the iframe

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,9 +1,14 @@
 import { useRef } from 'react'
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
+import codeShift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
 
-import './code-editor.css'
+import './css/code-editor.css';
+import './css/syntax.css'
+
 
 interface CodeEditorProps {
   initialValue: string;
@@ -17,7 +22,22 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue}) => {
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     })
-  monacoEditor.getModel()?.updateOptions({ tabSize: 2});
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2});
+
+    const highlighter = new Highlighter(
+      // @ts-ignore
+      window.monaco,
+      codeShift,
+      monacoEditor
+    );
+    
+    highlighter.highLightOnDidChangeModelContent(
+      () => {},
+      () => {},
+      undefined,
+      () => {}
+    );
   };
 
   const onFormatClick = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom';
 import CodeEditor from './components/code-editor';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import Preview from './components/preview';
+
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe= useRef<any>();
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
   const startService = async () => {
@@ -27,8 +30,6 @@ const App = () => {
       return
     }
 
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle:true,
@@ -43,31 +44,8 @@ const App = () => {
       }
     })
 
-    // console.log(result)
-
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-
+    setCode(result.outputFiles[0].text);
   }
-  const html = `
-    <html>
-    <head></head>
-    <body>
-      <div id="root"></div>
-      <script>
-      window.addEventListener('message', (event) => {
-        try {
-          eval(event.data);
-        } catch(err) {
-          const root = document.querySelector('#root');
-          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-          console.error(err);
-        }
-      }, false);
-      </script>
-    </body>
-    </html>
-  `;
 
   return (
   <div>
@@ -75,14 +53,10 @@ const App = () => {
       initialValue="const a = 1"
       onChange={(value) => setInput(value)}
     />
-    <textarea
-      value={input}
-      onChange={e => setInput(e.target.value)}
-      ></textarea>
     <div>
       <button onClick={onClick}>Submit</button>
     </div>
-    <iframe title="Preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
+    <Preview code={code}/>
   </div>
   );
 };
